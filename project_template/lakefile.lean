55fe5dbb8281112c094ab1302b51d466fc813199
ashleighwilcox <ashleigh.wilcox@yahoo.com>
2026-07-03 14:34:46 +0100

    update working?

--- project_template/lakefile.lean
+++ project_template/lakefile.lean
@@ -8,6 +8,7 @@ require verso from git "https://github.com/leanprover/verso"@"v4.31.0"
 -- Temporary v4.31-compatible pin to leanprover/verso-slides#59 for Config.extraHead.
 require «verso-slides» from git "https://github.com/ejgallego/verso-slides.git"@"e6a5d54228eb21fd86b041ab786d2d03bfb46685"
 require VersoBlueprint from git "https://github.com/leanprover/verso-blueprint"@"v4.31.0"
+require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.31.0"
 
 package DiophantineLean where
   precompileModules := false
